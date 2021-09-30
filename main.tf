be3b9ab0c8d5de171d56519b6115a48c30df7066
jenyadev <88274246+jenyadev@users.noreply.github.com>
2021-09-30 18:24:24 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,44 +11,14 @@ terraform {
 provider "docker" {}
 
 resource "docker_image" "nodered_image" {
-    name = "nodered/node-red:latest"
+  name = "nodered/node-red:latest"
 }
-
-resource "random_string" "random" {
-  count = 2
-  length = 4
-  special = false
-  upper = false
-}
-
-
 
 resource "docker_container" "nodered_container" {
-  count = 2  
-  name = join("-",["nodered", random_string.random[count.index].result])
+  name  = "nodered"
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
-    #external = 1880
-    }
-}
-
-output "IP-Adress" {
-  value = join(":", [docker_conteiner.nodered_container[0].ip_adress, docker_container.nodered_conteiner[0].ports[0].external])
-  description = "TheIP adress of the container"
-}
-
-uotput "container-name" {
-  value = docker_container.nodered_container[0].name
-  description = "The name of the container"
-}      
-
-output "IP-Adress" {
-  value = join(":", [docker_conteiner.nodered_container[1].ip_adress, docker_container.nodered_conteiner[1].ports[0].external])
-  description = "TheIP adress of the container"
-}
-
-uotput "container-name" {
-  value = docker_container.nodered_container[1].name
-  description = "The name of the container"  
+    external = 1880
+  }
 }
