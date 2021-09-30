825755a9a9b82585659ca834d0f7ab091131b5ae
jenyadev <88274246+jenyadev@users.noreply.github.com>
2021-09-30 21:45:58 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,11 +14,23 @@ resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
-resource "docker_container" "nodered_container" {
-  name  = "nodered"
+resource "docker_container" "nodered_container2" {
+  name  = "nodered2"
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
-    external = 1880
+    external = 1882
   }
 }
+
+resource "docker_image" "apache_image" {
+  name = "httpd:latest"
+}
+
+resource "docker_container" "apache_container" {
+  name  = "apache"
+  image = docker_image.apache_image.latest
+  ports {
+    internal = 80
+    external = 1880
+  }
